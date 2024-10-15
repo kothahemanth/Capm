a77de84fbe338da7c572b94ae9abae4955c86370
kothahemanth <hemanthkotha17@gmail.com>
2024-10-15 16:51:44 +0530

    Dialog Box Done

--- app/product/webapp/ext/xmlFetch/XmlFetch.js
+++ app/product/webapp/ext/xmlFetch/XmlFetch.js
@@ -1,25 +1,63 @@
 sap.ui.define([
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
-    "jquery.sap.global"
-], function (MessageToast, JSONModel, jQuery) {
+    "jquery.sap.global",
+    "sap/m/Dialog",
+    "sap/m/Text",
+    "sap/m/Button",
+    "sap/m/TextArea"
+], function (MessageToast, JSONModel, jQuery, Dialog, Text, Button, TextArea) {
     'use strict';
 
     return {
-        fetch : function (oEvent) {
+        fetch: function (oBindingContext, aSelectedContexts) {
+            // Create a status text element
+            var oStatusText = new Text({ text: "Starting to Fetch XML Data" });
+
+            // Create a TextArea for displaying the XML data
+            var oXMLDataTextArea = new TextArea({
+                width: "100%",
+                rows: 10,
+                editable: false, 
+                value: "" 
+            });
+
+            var oDialog = new Dialog({
+                title: "Fetching Details",
+                content: [oStatusText, oXMLDataTextArea],
+                beginButton: new Button({
+                    text: "Cancel",
+                    press: function () {
+                        oDialog.close();
+                    }
+                }),
+                endButton: new Button({
+                    text: "OK",
+                    press: function () {
+                        oDialog.close();
+                    }
+                })
+            });
+
+            oDialog.open();
+
             jQuery.ajax({
-                url: "/odata/v4/nnrg/productData", 
+                url: "/odata/v4/nnrg/productData",
                 method: "POST",
                 contentType: "application/json",
-                
+
                 success: function (oData) {
-                    console.log("XML DATA : ",oData);
-                    
+                    console.log("XML DATA: ", oData);
+
+                    oStatusText.setText("Billing Docs fetched successfully!");
+
+                    oXMLDataTextArea.setValue(JSON.stringify(oData, null, 2)); // Convert JSON to string
                 },
+
                 error: function (oError) {
-                    console.error("ERROR!!!!!!!!!!!");
-                    
-                }
+                    console.error("ERROR: ", oError);
+                    oStatusText.setText("Error fetching billing docs!");
+                },
             });
         }
     };
